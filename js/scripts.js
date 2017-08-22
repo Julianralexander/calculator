3ead4f3d47af2d33f14ba9186f05e999cb909734
Joe McKinney <mckinney.a.joe@gmail.com>
2017-08-22 11:43:06 -0700

    add all of my js for different math functions and will link it to the html page

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+
+
+var add = function(number1, number2){
+	return number1 + number2;
+};
+
+var minus = function(number1, number2){
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2){
+	return number1 * number2;
+};
+
+var divide = function(number1, number2){
+	return number1 / number2;
+};
+
+
+alert(minus(number1, number2));
